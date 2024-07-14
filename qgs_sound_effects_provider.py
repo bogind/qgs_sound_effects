0391565a572c41f152033467d1c127835f14282c
bogind <dror.bogin@gmail.com>
2024-07-14 14:43:25 +0300

    fixed provider algorithm not playing in modeler (which was the whole point)

--- qgs_sound_effects_provider.py
+++ qgs_sound_effects_provider.py
@@ -27,8 +27,7 @@ import os
 from qgis.PyQt.QtCore import QCoreApplication, QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from qgis.core import (QgsProcessingProvider, QgsProcessingAlgorithm, 
-                        QgsProcessingParameterNumber,
-                       QgsProcessingParameterEnum)
+                        QgsProcessingParameterNumber,QgsProcessingParameterEnum)
 
 from qgis.PyQt.QtGui import QIcon
 
@@ -42,27 +41,19 @@ class PlaySoundAlgorithm(QgsProcessingAlgorithm):
     LOOPS = 'LOOPS'
     OUTPUT = 'OUTPUT'
     
-
-    @staticmethod
-    def name():
+    def name(self):
         return 'play_sound'
     
-    @staticmethod
-    def displayName():
+    def displayName(self):
         return 'Play Sound'
     
-    @staticmethod
-    def group() -> str:
-        """Return the group of the algorithm."""
+    def group(self):
         return ''
 
-    @staticmethod
-    def groupId() -> str:
-        """Return the id of the group."""
+    def groupId(self):
         return ''
     
-    @staticmethod
-    def shortHelpString():
+    def shortHelpString(self):
         return 'Play a sound effect'
     
     def icon(self):
@@ -93,38 +84,49 @@ class PlaySoundAlgorithm(QgsProcessingAlgorithm):
             )
         params = [sound_param, volume_param]
         for param in params:
-            self.addParameter(param)
+            self.addParameter(param,
+                createOutput = True)
+            
+
+    def prepareAlgorithm(self, parameters, context, feedback):
+        self.play_sound = self.parameterAsEnum(parameters, self.SOUND, context)
+        self.play_volume = self.parameterAsDouble(parameters, self.VOLUME, context)
+
+        keys = self.sounds_config.keys()
+        self.play_sound = self.sounds_config[list(keys)[self.play_sound]]
+        self.file_path = os.path.join(self.plugin_dir, self.play_sound['filename'])
+        self.player = QMediaPlayer()
+        feedback.pushInfo('Playing sound effect: {} at volume {}'.format(self.play_sound['label'], self.play_volume))
+
+        self.player = QMediaPlayer()
+        self.player.setVolume(int(self.play_volume*100))
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(self.file_path)))
+        self.player.audioAvailableChanged.connect(lambda: self.player.play())
+        return super().prepareAlgorithm(parameters, context, feedback)
+
 
     def processAlgorithm(self, parameters, context, feedback):
         try:
-            #sound = parameters[self.SOUND]
-            sound = self.parameterAsEnum(parameters, self.SOUND, context)
-            volume = self.parameterAsDouble(parameters, self.VOLUME, context)
-
-            keys = self.sounds_config.keys()
-            sound = self.sounds_config[list(keys)[sound]]
-            file_path = os.path.join(self.plugin_dir, sound['filename'])
-
-            feedback.pushInfo('Playing sound effect: {} at volume {}'.format(sound['label'], volume))
-
-            self.player = QMediaPlayer()
-            self.player.setVolume(int(volume*100))
-            self.player.setMedia(QMediaContent(QUrl.fromLocalFile(file_path)))
-            self.player.play()
-
-
-        except Exception as e:
-            print(e)
-            return {self.OUTPUT: 'Failed to play sound effect'}
-
-
-        return {self.OUTPUT:{
-            'SOUND': sound['label'], 
-            'VOLUME': volume, 
+            # will not be triggered in most cases as the sound should have not been loaded yet
+            # but just in case
+            if not self.player.isAudioAvailable():
+                self.player.setMedia(QMediaContent(QUrl.fromLocalFile(self.file_path)))
+                self.player.audioAvailableChanged.connect(lambda: self.player.play())
+                self.player.setVolume(int(self.play_volume*100))
+                self.player.play()
+            else:
+                self.player.play()
+            feedback.pushInfo('Sound effect played')
+            return {self.OUTPUT:{
+            'SOUND': self.play_sound['label'], 
+            'VOLUME': self.play_volume, 
             'OUTPUT': 'Played Sound Effect'}
             }
-    
 
+        except Exception as e:
+            return {self.OUTPUT: 'Failed to play sound effect', 'ERROR': str(e)}
+            
+    
     def createInstance(self):
         return PlaySoundAlgorithm()
     
